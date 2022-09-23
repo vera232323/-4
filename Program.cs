446aa7d7afc1e0e3fde9fabc983705a69d6de0a6
Vera <imbesom@mail.ru>
2022-09-23 13:43:12 +0300

    код

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+int c = 1;
+while(c<=i)
+ { if(c%2 == 0) 
+  {
+   Console.WriteLine("Четное число - " + c);
+  } 
+  c++;
+ }
+  
